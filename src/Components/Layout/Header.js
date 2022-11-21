ddee9b613bd7feae2c8132c005da0a1ae0213357
Ayush Sagar <70971035+itsmeAYUSH@users.noreply.github.com>
2022-11-21 16:34:40 +0530

    Add files via upload

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -3,13 +3,13 @@ import mealsImage from "../../assets/meals.jpg";
 import classes from "./Header.module.css";
 import HeaderCartButton from "./HeaderCartButton";
 
-const Header = () => {
+const Header = props => {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <HeaderCartButton />
-      </header>
+        <HeaderCartButton onClick={props.onShowCart}/>
+      </header> 
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="delicious food" />
       </div>
--- src/Components/Layout/HeaderCartButton.js
+++ src/Components/Layout/HeaderCartButton.js
@@ -1,9 +1,9 @@
 import "./Cart/CartIcon";
 import CartIcon from "./Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
-const HeaderCartButton = () => {
+const HeaderCartButton = props => {
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
